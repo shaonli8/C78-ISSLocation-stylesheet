90bb7042f2333d1d86246e5ef5307703cf51f89b
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-11-13 15:42:32 +0530

    Create issinfo.js

---
+++ issinfo.js
@@ -0,0 +1,15 @@
+const styles = StyleSheet.create({
+    infoContainer: {
+        flex: 0.2,
+        backgroundColor: 'white',
+        marginTop: -10,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 30
+    },
+    infoText: {
+        fontSize: 15,
+        color: "black",
+        fontWeight: "bold"
+    }
+});
